91e739ea984c5bcc4269fb1998ff901b0cbc02eb
wrapperup <wrapperup4@gmail.com>
2023-08-19 05:09:01 -0400

    macro to automatically select the correct service

--- creme_macros/src/service.rs
+++ creme_macros/src/service.rs
@@ -5,9 +5,20 @@ use quote::quote;
 pub fn service(input: TokenStream) -> syn::Result<TokenStream> {
     let quoted = if let Ok(env) = std::env::var("CREME_RELEASE_MODE") {
         if env == "release" {
-            input
+            // TODO: Not implemented yet. This handles embedded assets.
+            quote! {
+                ::creme::services::CremeDevService::new(
+                    ::std::path::PathBuf::from(::core::env!("CREME_ASSETS_DIR")),
+                    ::std::path::PathBuf::from(::core::env!("CREME_PUBLIC_DIR"))
+                )
+            }
         } else {
-            input
+            quote! {
+                ::creme::services::CremeDevService::new(
+                    ::std::path::PathBuf::from(::core::env!("CREME_ASSETS_DIR")),
+                    ::std::path::PathBuf::from(::core::env!("CREME_PUBLIC_DIR"))
+                )
+            }
         }
     } else {
         return Err(syn::Error::new(
@@ -16,5 +27,5 @@ pub fn service(input: TokenStream) -> syn::Result<TokenStream> {
         ));
     };
 
-    Ok(quoted)
+    Ok(quoted.into())
 }
